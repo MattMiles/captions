8d7d5f5f0fa6f36e38aaa9597a23507f5ebce01a
Matthew Miles <matthewryanmiles@gmail.com>
2018-07-22 23:51:54 -0700

    Identify YouTube video ids in submission stream

--- captions/main.py
+++ captions/main.py
@@ -12,6 +12,26 @@ def get_subreddits_from_config():
             sys.exit(1)
 
 def main():
-    reddit = praw.Reddit('captions', user_agent='reddit-captions-bot')
+    reddit = praw.Reddit('captions-reddit', user_agent='reddit-captions-bot')
+
+    subreddits = get_subreddits_from_config()
+    subreddit = reddit.subreddit('+'.join(subreddits))
+
+    # compatible YouTube URL formats:
+    # https://www.youtube.com/watch?v=###########
+    # https://youtu.be/###########
+    youtube_pattern = re.compile(
+        r'^(https?:\/\/)?(((www\.|m\.)?youtube.com\/watch(\?v=(?P<id>.{11}))|(\?.+=.+))|((www\.)?youtu\.be\/(?P<shortened_id>.{11})(.+=.+)?))$'
+    )
+
+    for submission in subreddit.stream.submissions():
+        try:
+            match = re.match(youtube_pattern, submission.url)
+
+            if match:
+                video_id = match.group('id') or match.group('shortened_id')
+                
+        except praw.exceptions.PRAWException as e:
+            print(e)
 
 main()
