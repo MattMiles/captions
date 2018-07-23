543360493787a5ac5cd10e2420f90aae1308f22f
Matthew Miles <matthewryanmiles@gmail.com>
2018-07-22 21:04:06 -0700

    Add function to get user specified subreddits from praw.ini

--- captions/main.py
+++ captions/main.py
@@ -1,5 +1,15 @@
+import re
+import sys
 import praw
-import requests
+
+def get_subreddits_from_config():
+    with open('praw.ini') as config:
+        try:
+            return re.search(r'\nsubreddits=(([\w]+,?)+)', config.read()) \
+                .group(1).split(',')
+        except AttributeError:
+            print('Error: missing subreddits in praw.ini. See README.md for more details.')
+            sys.exit(1)
 
 def main():
     reddit = praw.Reddit('captions', user_agent='reddit-captions-bot')
