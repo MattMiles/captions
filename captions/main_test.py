5c73feb5a30c59610212632bc85939babbd03927
Matthew Miles <matthewryanmiles@gmail.com>
2018-07-24 22:07:41 -0700

    Refactor main.py unit tests into individual calls

--- captions/main_test.py
+++ captions/main_test.py
@@ -1,34 +1,30 @@
 import unittest
 
-import main
+from main import is_youtube_url
 
 class TestMain(unittest.TestCase):
     def test_is_youtube_url(self):
         # base URLs
-        self.assertTrue(
-            main.is_youtube_url('https://www.youtube.com/watch?v=mW6hFttt_KE') and
-            main.is_youtube_url('https://youtube.com/watch?v=mW6hFttt_KE') and
-            main.is_youtube_url('http://www.youtube.com/watch?v=mW6hFttt_KE') and
-            main.is_youtube_url('http://youtube.com/watch?v=v=mW6hFttt_KE') and
-            main.is_youtube_url('www.youtube.com/watch?v=mW6hFttt_KE') and
-            main.is_youtube_url('youtube.com/watch?v=v=mW6hFttt_KE') and
-            main.is_youtube_url('https://youtu.be/mW6hFttt_KE') and
-            main.is_youtube_url('http://youtu.be/mW6hFttt_KE') and
-            main.is_youtube_url('youtu.be/mW6hFttt_KE')
-        )
+        self.assertTrue(is_youtube_url('https://www.youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('https://youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('http://www.youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('http://youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('www.youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('youtube.com/watch?v=mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('https://youtu.be/mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('http://youtu.be/mW6hFttt_KE'))
+        self.assertTrue(is_youtube_url('youtu.be/mW6hFttt_KE'))
 
         # with params
-        self.assertTrue(
-            main.is_youtube_url('https://www.youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('https://youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('http://www.youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('http://youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('www.youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('youtube.com/watch?v=mW6hFttt_KE&t=23') and
-            main.is_youtube_url('https://youtu.be/mW6hFttt_KE?t=23') and
-            main.is_youtube_url('http://youtu.be/mW6hFttt_KE&t=23') and
-            main.is_youtube_url('youtu.be/mW6hFttt_KE&t=23')
-        )
+        self.assertTrue(is_youtube_url('https://www.youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('https://youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('http://www.youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('http://youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('www.youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('youtube.com/watch?v=mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('https://youtu.be/mW6hFttt_KE?t=23'))
+        self.assertTrue(is_youtube_url('http://youtu.be/mW6hFttt_KE&t=23'))
+        self.assertTrue(is_youtube_url('youtu.be/mW6hFttt_KE&t=23'))
 
 if __name__ == '__main__':
     unittest.main()
