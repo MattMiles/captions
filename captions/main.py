69524162f564f2583243e4796975e94aa62baa11
Matthew Miles <matthewryanmiles@gmail.com>
2018-07-24 21:55:52 -0700

    Refactor unnecessary global constant

--- captions/main.py
+++ captions/main.py
@@ -2,13 +2,6 @@ import re
 import sys
 import praw
 
-# compatible YouTube URL formats:
-# https://www.youtube.com/watch?v=###########
-# https://youtu.be/###########
-YOUTUBE_PATTERN = re.compile(
-    r'^(https?:\/\/)?(((www\.|m\.)?youtube.com\/watch(\?v=(?P<id>.{11}))|(\?.+=.+))|((www\.)?youtu\.be\/(?P<shortened_id>.{11})(.+=.+)?))$'
-)
-
 def get_subreddits_from_config():
     with open('praw.ini') as config:
         try:
@@ -19,6 +12,12 @@ def get_subreddits_from_config():
             sys.exit(1)
 
 def is_youtube_url(url):
+    # compatible YouTube URL formats:
+    # https://www.youtube.com/watch?v=###########
+    # https://youtu.be/###########
+    YOUTUBE_PATTERN = re.compile(
+        r'^(https?:\/\/)?(((www\.|m\.)?youtube.com\/watch(\?v=(?P<id>.{11}))|(\?.+=.+))|((www\.)?youtu\.be\/(?P<shortened_id>.{11})(.+=.+)?))$'
+    )
     return re.match(YOUTUBE_PATTERN, url)
 
 def get_youtube_video_id(url_match):
